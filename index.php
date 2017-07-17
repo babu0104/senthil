d77bbdfc945047d248b7df780045bfe2a2350777
senthilkumark <senthilkumarkindia@gmail.com>
2017-07-18 02:10:44 +0530

    I have inserted index.php

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+require_once('../config/config.php');
+ $page = 'Hotels';
+ if (!isset($_SESSION['IMAGINETRIP_LOGID']))
+{
+
+$path_logout = "../";
+$user = new User();
+ $user->doLogout($path_logout);
+ 
+}
+require_once('../header.php');
+
+?>
+
+
+            <!--BEGIN CHAT FORM-->
+            <div id="chat-form" class="fixed">
+                <div class="chat-inner">
+                    <h2 class="chat-header">
+                        <a href="javascript:;" class="chat-form-close pull-right"><i class="glyphicon glyphicon-remove">
+                        </i></a><i class="fa fa-user"></i>&nbsp; Chat &nbsp;<span class="badge badge-info">3</span></h2>
+                    <div id="group-1" class="chat-group">
+                        <strong>Favorites</strong><a href="#"><span class="user-status is-online"></span> <small>
+                            Verna Morton</small> <span class="badge badge-info">2</span></a><a href="#"><span
+                                class="user-status is-online"></span> <small>Delores Blake</small> <span class="badge badge-info is-hidden">
+                                    0</span></a><a href="#"><span class="user-status is-busy"></span> <small>Nathaniel Morris</small>
+                                        <span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-idle"></span>
+                                            <small>Boyd Bridges</small> <span class="badge badge-info is-hidden">0</span></a><a
+                                                href="#"><span class="user-status is-offline"></span> <small>Meredith Houston</small>
+                                                <span class="badge badge-info is-hidden">0</span></a></div>
+                    <div id="group-2" class="chat-group">
+                        <strong>Office</strong><a href="#"><span class="user-status is-busy"></span> <small>
+                            Ann Scott</small> <span class="badge badge-info is-hidden">0</span></a><a href="#"><span
+                                class="user-status is-offline"></span> <small>Sherman Stokes</small> <span class="badge badge-info is-hidden">
+                                    0</span></a><a href="#"><span class="user-status is-offline"></span> <small>Florence
+                                        Pierce</small> <span class="badge badge-info">1</span></a></div>
+                    <div id="group-3" class="chat-group">
+                        <strong>Friends</strong><a href="#"><span class="user-status is-online"></span> <small>
+                            Willard Mckenzie</small> <span class="badge badge-info is-hidden">0</span></a><a
+                                href="#"><span class="user-status is-busy"></span> <small>Jenny Frazier</small>
+                                <span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-offline"></span>
+                                    <small>Chris Stewart</small> <span class="badge badge-info is-hidden">0</span></a><a
+                                        href="#"><span class="user-status is-offline"></span> <small>Olivia Green</small>
+                                        <span class="badge badge-info is-hidden">0</span></a></div>
+                </div>
+                <div id="chat-box" style="top: 400px">
+                    <div class="chat-box-header">
+                        <a href="#" class="chat-box-close pull-right"><i class="glyphicon glyphicon-remove">
+                        </i></a><span class="user-status is-online"></span><span class="display-name">Willard
+                            Mckenzie</span> <small>Online</small>
+                    </div>
+                    <div class="chat-content">
+                        <ul class="chat-box-body">
+                            <li>
+                                <p>
+                                    <img src="images/avatar/128.jpg" class="avt" /><span class="user">John Doe</span><span
+                                        class="time">09:33</span></p>
+                                <p>
+                                    Hi Swlabs, we have some comments for you.</p>
+                            </li>
+                            <li class="odd">
+                                <p>
+                                    <img src="images/avatar/48.jpg" class="avt" /><span class="user">Swlabs</span><span
+                                        class="time">09:33</span></p>
+                                <p>
+                                    Hi, we're listening you...</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="chat-textarea">
+                        <input placeholder="Type your message" class="form-control" /></div>
+                </div>
+            </div>
+            <!--END CHAT FORM-->
+            <!--BEGIN PAGE WRAPPER-->
+            <div id="page-wrapper">
+                <!--BEGIN TITLE & BREADCRUMB PAGE-->
+                <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
+                    <div class="page-header pull-left">
+                        <div class="page-title">
+                            Hotels</div>
+                    </div>
+                    <ol class="breadcrumb page-breadcrumb pull-right">
+                        <li><i class="fa fa-home"></i>&nbsp;<a href="<?php echo $page_link; ?>dashboard/index.php">Home</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
+                        <li class="hidden"><a href="<?php echo $page_link; ?>Hotels/index.php">Hotels</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
+                        <li class="active">Hotels</li>
+                    </ol>
+                    <div class="clearfix">
+                    </div>
+                </div>
+                <!--END TITLE & BREADCRUMB PAGE-->
+               
+                <!--BEGIN CONTENT-->
+                <div class="page-content">
+                    <div id="tab-general">
+                          <h1>Welcome to Hotels Information</h1>
+          
+                        </div>
+                    </div>
+                </div>
+                <!--END CONTENT-->
+     
+<?php
+require_once('../footer.php');
+?>
+
+
+
